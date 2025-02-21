f736b34a320da62b611923c066c00b04ee7c7ca8
heethasha <heethasha@gmail.com>
2025-02-21 11:18:42 +1100

    Initial Commit

---
+++ StudentList/StudentListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudentListApp.swift
+//  StudentList
+//
+//  Created by Heethasha on 21/2/2025.
+//
+
+import SwiftUI
+
+@main
+struct StudentListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
